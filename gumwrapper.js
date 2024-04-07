ee8e8a2e2d1cafdce73ef216f4d5c474aa9b3651
randy2014 <1247657199@qq.com>
2024-04-07 21:54:26 +0800

    Update gumwrapper.js

--- gumwrapper.js
+++ gumwrapper.js
@@ -1,82 +1,75 @@
-/*!
- * GumWrapper v0.13
- * By Daniel Davis under MIT License
- * https://github.com/tagawa/GumWrapper
- */
+<!doctype html>
 
-;(function (window, document) {
-    'use strict';
-    
-    window.GumWrapper = function(elements, success, error) {
-        // Define our error message
-        function sendError(message) {
-            if (error) {
-                var e = new Error();
-                e.message = message;
-                error(e);
-            } else {
-                console.error(message);
-            }
-        }
-        
-        // Try to play the media stream
-        function play() {
-            var video = document.getElementById(elements.video);
-            if (!video) {
-                sendError('Unable to find the video element.');
-                return;
-            }
 
-            function successCallback(stream) {
-                // Set the source of the video element with the stream from the camera
-                if (video.mozSrcObject !== undefined) {
-                    video.mozSrcObject = stream;
-                } else {
-                    video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-                }
-                video.play();
-            }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Custom File Input Button</title>
+<style>
+    .file-upload-wrapper {
+        position: relative;
+        overflow: hidden;
+        display: inline-block;
+		width: 50%; /* 或者具体的宽度值 */
+    }
+    .file-upload-input {
+        font-size: 100px;
+        position: absolute;
+        left: 0;
+        top: 0;
+        opacity: 0;
+    }
+    .file-upload-button {
+        display: inline-block;
+        cursor: pointer;
+		font-size: 50px;
+		 
+    }
+	
+	/* 父容器需要设置为flex布局 */
+.center-container {
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
+  height: 100vh; /* 父容器的高度设置为视窗高度 */
+}
+ 
+ 
+</style>
+</head>
+<body>
+ <div class ="center-container">
+<div class="file-upload-wrapper">
+    <button type="button" class="file-upload-button">H5打开相册或者拍照</button>
+    <input type="file" accept="image/*"  class="file-upload-input" onchange="uploadFile(this)" />
+	<br/>
+	 <img id="imageDisplay" src="" alt="Image preview...">
+	 
+	<br/>
+	 <br/>
+	 <label id="selectFileName">已选择:</label>	 <br/>
+ 
+</div>
+ </div>
+<script>
 
-            function errorCallback(error) {
-                sendError('Unable to get webcam stream.');
-            }
 
-            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-            window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+   
 
-            // Call the getUserMedia method with our callback functions
-            if (navigator.getUserMedia) {
-                navigator.getUserMedia({video: true}, successCallback, errorCallback);
-            } else {
-                sendError('Native web camera streaming (getUserMedia) not supported in this browser.');
-            }
-            
-            // Check the video dimensions when data has loaded
-            video.addEventListener('loadeddata', function() {
-                var attempts = 10;
-                
-                function checkVideo() {
-                    if (attempts > 0) {
-                        if (video.videoWidth > 0 && video.videoHeight > 0) {
-                            // Execute success callback function
-                            if (success) success(video);
-                        } else {
-                            // Wait a bit and try again
-                            window.setTimeout(checkVideo, 500);
-                        }
-                    } else {
-                        // Give up after 10 attempts
-                        sendError('Unable to play video stream. Is webcam working?');
-                    }
-                    attempts--;
-                }
-                
-                checkVideo();
-            }, false);
-        }
-
-        return {
-            play: play
-        };
-    };
-})(window, document);
+function uploadFile(input) {
+    if (input.files && input.files[0]) {
+	    document.getElementById('selectFileName').innerText  =input.files[0].name;
+		 var file = e.target.files[0];
+		 var reader = new FileReader();
+		 reader.onload = function(e) {
+			document.getElementById('imageDisplay').src = e.target.result;
+		 };
+    }
+}
+ 
+</script>
+ 
+</body>
+</html>
+ 
